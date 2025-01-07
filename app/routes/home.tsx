d99be0e1b21f3cd44e2e0492a5f6144d850e1a9f
Blissito <fixtergeek@gmail.com>
2025-01-07 10:50:32 -0600

    direct uploads ready

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -4,8 +4,10 @@ import { CopyButton } from "~/components/common/CopyButton";
 import { useRef, type ChangeEvent } from "react";
 import { useUpload } from "~/hooks/useUpload";
 import { db } from "~/.server/db";
-import { FaSpinner } from "react-icons/fa";
+import { FaRegCheckCircle, FaSpinner } from "react-icons/fa";
 import { useSubmit } from "react-router";
+import { LuFileWarning } from "react-icons/lu";
+import { cn } from "~/utils/cn";
 
 export function meta() {
   return [
@@ -59,10 +61,14 @@ export default function Home({ loaderData }: Route.ComponentProps) {
       </section>
       <section>
         <nav className="w-full bg-indigo-500 flex items-center justify-between px-4 py-2">
-          <h2>Tabla de assets</h2>
+          <h2>Todos tus assets</h2>
           <button
+            disabled={isFetching}
             onClick={handleAssetSelection}
-            className="bg-indigo-800 p-3 rounded-2xl"
+            className={cn(
+              "bg-indigo-800 p-3 rounded-2xl",
+              "disabled:pointer-events-none disabled:bg-gray-500"
+            )}
           >
             {isFetching ? (
               <p className="animate-spin">
@@ -81,9 +87,25 @@ export default function Home({ loaderData }: Route.ComponentProps) {
         </nav>
         <>
           {assets.map((asset) => (
-            <div className="p-3 bg-gray-900 grid grid-cols-2" key={asset.id}>
-              <span>{asset.storageKey}</span>
-              <span>{asset.status}</span>
+            <div
+              className="p-3 bg-gray-900 grid grid-cols-4 text-xs"
+              key={asset.id}
+            >
+              <span className="col-span-2">{asset.storageKey}</span>
+              <p className="flex gap-1 items-center">
+                {asset.status}
+
+                {asset.status === "uploaded" ? (
+                  <span className="text-green-300">
+                    <FaRegCheckCircle />
+                  </span>
+                ) : (
+                  <span className="text-yellow-300">
+                    <LuFileWarning />
+                  </span>
+                )}
+              </p>
+              <span>{asset.contentType}</span>
             </div>
           ))}
         </>
