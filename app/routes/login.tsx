49571465e05e13cdb76f00ab5f38740c51801e36
oswaldo  martinez <oswalfut_96@hotmail.com>
2025-01-19 13:15:10 -0500

    add motion

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,8 +1,6 @@
 import { redirect } from "react-router";
 import type { Route } from "./+types/login";
 import { createGoogleSession, getGoogleURL } from "~/.server/getters";
-import LoginComponent from "~/components/login/login-component";
-import { getSession } from "~/.server/sessions";
 
 export const loader = async ({ request }: Route.LoaderArgs) => {
   const url = new URL(request.url);
@@ -10,6 +8,5 @@ export const loader = async ({ request }: Route.LoaderArgs) => {
   if (code) {
     await createGoogleSession(code, request);
   }
-  const session = await getSession(request.headers.get("Cookie"));
-  if (session.has("email")) throw redirect("/");
+  return redirect(getGoogleURL());
 };
