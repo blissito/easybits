07b575bfa928eddb5efce3c1fa3cc1464502594a
Blissito <fixtergeek@gmail.com>
2025-02-04 10:04:16 -0600

    magicWand illustration added

--- app/routes/assets.tsx
+++ app/routes/assets.tsx
@@ -5,43 +5,39 @@ import { MagicWand } from "~/components/illustrations/MagicWand";
 
 export default function Assets() {
   const inputRef = useRef<HTMLInputElement>(null);
-  const handleFile = () => {};
+  const handleFile = () => {}; // @todo
   return (
     <>
       <article className="py-20 px-10 min-h-screen w-full relative box-border inline-block">
         <GridBackground />
         <h1 className="text-3xl relative z-20">Mis assets digitales</h1>
-        <section className="z-20 relative grid place-content-center place-items-center min-h-[70vh]">
-          <Empty />
-          <h2 className="text-2xl font-bold mb-4">Agrega tu primer producto</h2>
-          <p className="text-lg text-brand-gray text-center mb-10">
-            No lo pienses más, no tiene que se perfecto. <br />
-            ¡No sabrás si funciona si no lo intentas!
-          </p>
-
-          <BrutalButton
-            onClick={() => inputRef.current?.click()}
-            className="bg-brand-500"
-          >
-            + Agregar
-          </BrutalButton>
-
-          <input
-            ref={inputRef}
-            type="file"
-            className="hidden"
-            onChange={handleFile}
-          />
-        </section>
+        <Empty onClick={() => inputRef.current?.click()} />
+        <input
+          ref={inputRef}
+          type="file"
+          className="hidden"
+          onChange={handleFile}
+        />
       </article>
     </>
   );
 }
 
-const Empty = () => {
+const Empty = ({ onClick }: { onClick?: () => void }) => {
   return (
-    <div className="mb-10">
-      <MagicWand />
-    </div>
+    <section className="z-20 relative grid place-content-center place-items-center min-h-[70vh]">
+      <div className="mb-10">
+        <MagicWand />
+      </div>
+      <h2 className="text-2xl font-bold mb-4">Agrega tu primer producto</h2>
+      <p className="text-lg text-brand-gray text-center mb-10">
+        No lo pienses más, no tiene que se perfecto. <br />
+        ¡No sabrás si funciona si no lo intentas!
+      </p>
+
+      <BrutalButton onClick={onClick} className="bg-brand-500">
+        + Agregar
+      </BrutalButton>
+    </section>
   );
 };
