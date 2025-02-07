40f9644732761fcb2db44256fc047edb528b9ab7
oswaldo  martinez <oswalfut_96@hotmail.com>
2025-02-06 20:54:12 -0500

    fix2 on icons not loading

--- app/components/DashLayout/DashLayout.constants.ts
+++ app/components/DashLayout/DashLayout.constants.ts
@@ -1,12 +1,12 @@
-import RocketIcon from "~/assets/icons/rocket.svg";
-import StatsIcon from "~/assets/icons/stonks.svg";
-import AssetsIcon from "~/assets/icons/magic-box.svg";
-import StoreIcon from "~/assets/icons/laptop-and-mobile.svg";
-import MoneyIcon from "~/assets/icons/money.svg";
-import BagIcon from "~/assets/icons/purchase.svg";
-import UsersIcon from "~/assets/icons/users.svg";
-import UserIcon from "~/assets/icons/profile.svg";
-import LogoutIcon from "~/assets/icons/log-out.svg";
+import RocketIcon from "~/assets/icons/rocket.svg?url";
+import StatsIcon from "~/assets/icons/stonks.svg?url";
+import AssetsIcon from "~/assets/icons/magic-box.svg?url";
+import StoreIcon from "~/assets/icons/laptop-and-mobile.svg?url";
+import MoneyIcon from "~/assets/icons/money.svg?url";
+import BagIcon from "~/assets/icons/purchase.svg?url";
+import UsersIcon from "~/assets/icons/users.svg?url";
+import UserIcon from "~/assets/icons/profile.svg?url";
+import LogoutIcon from "~/assets/icons/log-out.svg?url";
 
 export const ITEMS = {
   navItems: [
--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,4 @@ export default defineConfig({
     },
   },
   plugins: [reactRouter(), tsconfigPaths()],
-  assetsInclude: ["**/*.svg"], // Ensures SVGs are properly bundled
 });
