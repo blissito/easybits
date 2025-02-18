5dbdea06d19c468ea7d1b7aaeea36fb6975f3d45
BrendaOrtega <bremin11.20.93@gmail.com>
2025-02-18 17:27:08 -0600

    navbar fixes & getting user on all navbar routes

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,11 +1,17 @@
 import { AuthNav } from "~/components/login/auth-nav";
 import { Footer } from "~/components/common/Footer";
 import { BlogContent, BlogHeader } from "./blog/BlogList";
+import type { Route } from "./+types/blog";
 
-export default function Blog() {
+export const clientLoader = async () => {
+  const user = await fetch("/api/v1/user?intent=self").then((r) => r.json());
+  return { user };
+};
+export default function Blog({ loaderData }: Route.ComponentProps) {
+  const { user } = loaderData;
   return (
     <section className="overflow-hidden">
-      <AuthNav />
+      <AuthNav user={user} />
       <BlogHeader />
       <BlogContent />
       <Footer />
