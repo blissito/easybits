3d849b8e3e63398956cc18b9e0b49a9b8dca7636
BrendaOrtega <bremin11.20.93@gmail.com>
2025-02-19 15:30:15 -0600

    dash layout adjustments

--- app/routes/sales.tsx
+++ app/routes/sales.tsx
@@ -4,7 +4,7 @@ export default function Sales() {
   return (
     <>
       <article className=" min-h-screen w-full relative box-border inline-block">
-        <h2 className="text-4xl font-semibold">Sales</h2>
+        <h2 className="text-4xl font-semibold">Ventas</h2>
       </article>
     </>
   );
