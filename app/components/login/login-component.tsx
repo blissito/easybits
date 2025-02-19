344982d9c6ede8bf2f86cbf054b0b66a18d760f7
BrendaOrtega <bremin11.20.93@gmail.com>
2025-02-18 18:54:40 -0600

    login test

--- app/components/login/login-component.tsx
+++ app/components/login/login-component.tsx
@@ -45,7 +45,7 @@ export default function LoginComponent() {
       <AuthNav />
       <main
         className={
-          "w-screen h-[90vh] overflow-hidden border border-black  bg-contain bg-center bg-patternDark flex justify-center  items-center"
+          "w-screen h-fit min-h-screen px-10 overflow-y-hidden border border-black  bg-contain bg-center bg-patternDark flex justify-center  items-center"
         }
       >
         <div className="flex flex-col w-full items-center justify-center">
