34c1d54e6af335314c387310b036108f384e1ee8
BrendaOrtega <bremin11.20.93@gmail.com>
2025-03-19 20:56:27 -0600

    mobile bar almost ready

--- app/components/DashLayout/DashLayout.tsx
+++ app/components/DashLayout/DashLayout.tsx
@@ -1,9 +1,9 @@
 import clsx from "clsx";
-import { useEffect, useState, type ReactNode } from "react";
+import { use, useEffect, useState, type ReactNode } from "react";
 import Logo from "/icons/easybits-logo.svg";
 import { NavLink, Outlet, useLocation } from "react-router";
 import TextLogo from "/icons/easybits-logo-text.svg";
-import { AnimatePresence, motion } from "motion/react";
+import { animate, AnimatePresence, motion, useAnimate } from "motion/react";
 import { ITEMS } from "./DashLayout.constants";
 import { cn } from "~/utils/cn";
 import { getUserOrRedirect } from "~/.server/getters";
@@ -18,6 +18,7 @@ interface MenuItemProps {
   isOpen?: boolean;
   isLogo?: boolean;
   end?: boolean;
+  index?: number;
 }
 
 export const loader = async ({ request }: Route.LoaderArgs) => ({
@@ -29,7 +30,7 @@ export default function DashLayout({ loaderData }: Route.ComponentProps) {
   const [isOpen, setIsOpen] = useState<boolean>(true);
 
   return (
-    <main className="flex relative z-10">
+    <main className="flex relative z-10 min-h-screen">
       <SideBar />
       {/* <GridBackground /> */}
       <Outlet />
@@ -39,14 +40,18 @@ export default function DashLayout({ loaderData }: Route.ComponentProps) {
 
 const SideBar = () => {
   return (
-    <div>
+    <section>
+      <div className="absolute h-full right-4 bottom-0 z-50 block md:hidden">
+        <FoldMenu />
+      </div>
       <motion.div
-        className="bg-black sticky top-0 z-10 h-screen text-white flex flex-col justify-between items-center transition-all py-8"
+        className="bg-black hidden md:flex sticky top-0 z-10 h-screen text-white  flex-col justify-between items-center transition-all py-8"
         // initial={{ width: isOpen ? 240 : 88 }}
         whileHover={{ width: 240 }}
-        // transition={{ duration: 0.3, ease: "easeInOut" }}
+        transition={{ duration: 0.3, ease: "easeInOut" }}
         // onMouseEnter={() => setIsOpen(true)}
-        // onMouseLeave={() => setIsOpen(true)} // @todo fixed for now
+        // onMouseLeave={() => setIsOpen(true)}
+        // @todo fixed for now
       >
         <div className="w-full">
           <div className="px-5 mb-4">
@@ -97,7 +102,92 @@ const SideBar = () => {
           ))}
         </ul>
       </motion.div>
+    </section>
+  );
+};
+
+const FoldMenu = () => {
+  const [isFold, setIsFold] = useState(false);
+
+  const onClick = () => {
+    if (isFold) {
+      setIsFold(false);
+      animate("#drawer", { y: "-100%" }, { duration: 0.5, type: "tween" });
+    } else {
+      setIsFold(true);
+      animate("#drawer", { y: "0%" }, { duration: 0.5, type: "tween" });
+    }
+  };
+  return (
+    <div className="flex flex-col justify-end pb-4 items-end h-full gap-4 ">
+      <AnimatePresence>
+        {isFold && (
+          <>
+            {ITEMS.navItems
+              .concat(ITEMS.sectionItems)
+              .concat(ITEMS.bottomItems)
+              .filter((item) => item.index !== 3)
+              .map((item, key) => (
+                <FoldMenuItem key={key} {...item} />
+              ))}{" "}
+          </>
+        )}
+      </AnimatePresence>
+      <div className="w-12 h-12 bg-black rounded-full ">
+        <DashBurger onClick={onClick} isFold={isFold} />
+      </div>
     </div>
+  );
+};
+
+const FoldMenuItem = ({
+  path = "",
+  icon,
+  iconSize,
+  end,
+  index = 1,
+}: MenuItemProps) => {
+  return (
+    <NavLink end={end} to={path}>
+      <motion.button
+        initial={{ filter: "blur(4px)", y: 0, opacity: 0 }}
+        animate={{ filter: "blur(0px)", y: -10, opacity: 1 }}
+        exit={{ filter: "blur(4px)", y: 0, opacity: 0 }}
+        transition={{ delay: 0.08 * index }}
+        className="w-12 h-12 rounded-full bg-black grid place-content-center "
+      >
+        {icon}
+      </motion.button>
+    </NavLink>
+  );
+};
+
+const DashBurger = ({
+  isFold,
+  onClick,
+}: {
+  isFold?: boolean;
+  onClick?: () => void;
+}) => {
+  const [scope, animate] = useAnimate();
+  useEffect(() => {
+    if (isFold) {
+      animate("#top", { rotateZ: -135, y: 6, backgroundColor: "white" });
+      animate("#bottom", { rotateZ: 135, y: -5, backgroundColor: "white" });
+    } else {
+      animate("#top", { rotateZ: 0, y: 0, backgroundColor: "white" });
+      animate("#bottom", { rotateZ: 0, y: 0, backgroundColor: "white" });
+    }
+  }, [isFold]);
+  return (
+    <button
+      onClick={onClick}
+      ref={scope}
+      className="flex w-full h-full  items-center justify-center flex-col gap-2 relative "
+    >
+      <div id="top" className=" w-8 h-[3px]  rounded-full"></div>
+      <div id="bottom" className="w-8 h-[3px]  rounded-full"></div>
+    </button>
   );
 };
 
@@ -136,18 +226,7 @@ const MenuItem = ({
         {isLogo ? (
           <img src={icon} className={clsx(`w-[${iconSize || 32}px]`)} />
         ) : (
-          <div
-            className={clsx(`w-[${iconSize || 32}px]`, {
-              "bg-brand-500": isActive,
-              "bg-white": !isActive,
-            })}
-            style={{
-              WebkitMaskImage: `url('${icon}')`,
-              maskImage: `url('${icon}')`,
-              width: `${iconSize || 32}px`,
-              height: `${iconSize || 32}px`,
-            }}
-          />
+          <span>{icon}</span>
         )}
         {title && (
           <AnimatePresence initial={false}>
