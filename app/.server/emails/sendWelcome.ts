8cb5799b0f68cb6c4e630e033e5a01d3296e179e
Blissito <fixtergeek@gmail.com>
2025-04-16 17:28:56 -0600

    full magic link cicle

--- app/.server/emails/sendWelcome.ts
+++ app/.server/emails/sendWelcome.ts
@@ -3,6 +3,10 @@ import { getSesTransport } from "./sendgridTransport";
 import { welcomeEmail } from "./templates/welcomeEmail";
 
 export const sendWelcomeEmail = (email: string, displayName?: string) => {
+  const isDev = process.env.NODE_ENV === "development";
+  const location = isDev
+    ? "http://localhost:3000"
+    : "https://www.easybits.cloud";
   const magicToken = generateUserToken({ email, displayName }, "7d");
   const url = new URL(`${location}/api/v1/tokens`);
   url.searchParams.set("intent", "magic_link");
