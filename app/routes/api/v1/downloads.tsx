16d0e25b90ed712b460a659fc6010cf4bed6dc0f
Blissito <fixtergeek@gmail.com>
2025-04-22 08:55:50 -0600

    building with nanoid

--- app/routes/api/v1/downloads.tsx
+++ app/routes/api/v1/downloads.tsx
@@ -9,18 +9,15 @@ export const action = async ({ request }: Route.ActionArgs) => {
   const formData = await request.formData();
   const intent = formData.get("intent");
   if (intent === "generate_token") {
-    console.log("here");
+    // @todo auth? asset  => user
     const fileId = formData.get("fileId") as string;
-    const expInSecs = formData.get("expInSecs") as string;
-    const expiresIn = formData.get("expiresIn") as string;
+    const expInSecs = formData.get("expInSecs") || 60;
+    const expiresIn = formData.get("expiresIn") || expInSecs;
     const file = await db.file.findUnique({ where: { id: fileId } });
     if (!file || !file.storageKey)
       throw new Response("The file does not exist", { status: 404 });
 
-    const url = await getReadURL(
-      file.storageKey
-      // Number(expiresIn || expInSecs)
-    );
+    const url = await getReadURL(file.storageKey, Number(expiresIn));
     return { url };
   }
 
