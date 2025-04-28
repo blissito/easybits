c4c11d4c8ae4bfbe680016f5abf602edf6c5d6ca
Blissito <fixtergeek@gmail.com>
2025-04-27 19:55:23 -0600

    testing flow

--- app/.server/emails/sendWelcome.ts
+++ app/.server/emails/sendWelcome.ts
@@ -7,16 +7,12 @@ export const sendWelcomeEmail = (email: string, displayName?: string) => {
   const location = isDev
     ? "http://localhost:3000"
     : "https://www.easybits.cloud";
-  const magicToken = generateUserToken({ email, displayName }, "7d");
-  const url = new URL(`${location}/api/v1/tokens`);
-  url.searchParams.set("intent", "magic_link");
-  url.searchParams.set("token", magicToken);
   return getSesTransport()
     .sendMail({
       from: "EasyBits@easybits.cloud",
       subject: "Bienvenid@ a EasyBits 🎉 📻",
       bcc: [email],
-      html: welcomeEmail({ displayName, link: url.toString() }),
+      html: welcomeEmail({ displayName, link: `${location}/dash` }),
     })
     .then((result: unknown) => console.log(result))
     .catch((e: Error) => console.error(e));
