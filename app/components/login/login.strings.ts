4fe404e2eef0d7fbbeea21ab01bd1b1db62fb9e5
Blissito <fixtergeek@gmail.com>
2025-05-01 15:36:45 -0600

    login fix

--- app/components/login/login.strings.ts
+++ app/components/login/login.strings.ts
@@ -2,17 +2,17 @@ export const STRINGS = {
   login: {
     title: "¡Ya te extrañabamos! \u000A Inicia sesión",
     formTitle: "Completa tus datos para entrar a tu cuenta ¡de una!",
-    actionQuestion: "¿No tienes cuenta? ",
+    actionQuestion: "¿No tienes cuenta?",
     action: "Crea una cuenta",
     formAction: "Iniciar sesión una red social.",
     formSubmit: "Iniciar sesión",
   },
   signup: {
     title: "¡Vamos a empezar! \u000A Abre una cuenta",
-    formTitle: "Completa tus datos \n y obten tu llave",
+    formTitle: "Solo un par de datos \n",
     actionQuestion: "¿Ya tienes cuenta? ",
     action: "Inicia sesión",
-    formAction: "Abre tu cuenta con una red social.",
-    formSubmit: "Abrir mi cuenta",
+    formAction: "Crea tu cuenta con una red social.",
+    formSubmit: "Crear mi cuenta",
   },
 };
