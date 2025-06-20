b2792928cc67eeff559e5204cbad8d8cbcd5dde6
blissito <blissitos@gmail.com>
2025-06-19 18:57:42 -0600

    testing ollama in prod

--- app/.server/llms/devstral.ts
+++ app/.server/llms/devstral.ts
@@ -1,13 +1,10 @@
 export const fetchInternalOllama = (prompt: string) => {
-  return fetch(
-    "http://ollama-blissmo-billowing-water-953.flycast/api/generate",
-    {
-      method: "post",
-      body: new URLSearchParams({
-        prompt,
-        stream: "false",
-        model: "devstral:24b-small-2505-q8_0",
-      }),
-    }
-  );
+  return fetch("http://ollama-old.flycast/api/generate", {
+    method: "post",
+    body: JSON.stringify({
+      prompt,
+      stream: "false",
+      model: "devstral:24b-small-2505-q8_0",
+    }),
+  });
 };
