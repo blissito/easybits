3891d14c1c759cd661d3e6ce6e5c0f8650bccb41
BrendaOrtega <bremin11.20.93@gmail.com>
2025-06-25 17:35:00 -0600

    public store default meta tags

--- app/routes/assets/publicStore.tsx
+++ app/routes/assets/publicStore.tsx
@@ -4,6 +4,7 @@ import type { Route } from "./+types/publicStore";
 import { db } from "~/.server/db";
 import type { Asset } from "@prisma/client";
 import { StoreTemplate } from "../store/storeTemplate";
+import getBasicMetaTags from "~/utils/getBasicMetaTags";
 
 export const loader = async ({ request }: Route.LoaderArgs) => {
   const url = new URL(request.url);
@@ -18,7 +19,10 @@ export const loader = async ({ request }: Route.LoaderArgs) => {
   });
 
   let assets: Asset[] = [];
+  let user = null;
+  
   if (hostExists) {
+    user = hostExists;
     assets = await db.asset.findMany({
       where: {
         userId: hostExists.id,
@@ -30,6 +34,7 @@ export const loader = async ({ request }: Route.LoaderArgs) => {
     });
   }
   if (domainExists) {
+    user = domainExists;
     assets = await db.asset.findMany({
       where: {
         userId: domainExists.id,
@@ -41,7 +46,22 @@ export const loader = async ({ request }: Route.LoaderArgs) => {
     });
   }
 
-  return { assets };
+  return { assets, user };
+};
+
+export const meta = ({
+  data,
+}: Route.MetaArgs) => {
+  // Get user info from the data
+  const user = data?.user;
+  
+  return getBasicMetaTags({
+    title: (user as any)?.storeConfig?.metadata?.title || user?.displayName || "Creador EasyBits",
+    description: (user as any)?.storeConfig?.metadata?.description || 
+      `Descubre increíbles assets digitales en la tienda de ${user?.displayName || "este creador"} 🚀`,
+    // @todo get this from config?
+    image: (user as any)?.storeConfig?.metadata?.image || user?.storeConfig?.coverImage|| user?.storeConfig?.logoImage || `/metaImage-tienda.webp`,
+  });
 };
 
 export default function Page({ loaderData }: Route.ComponentProps) {
