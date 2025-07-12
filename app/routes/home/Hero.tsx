3ade70d2488a7988a72814d5f58aae1bbe4b715c
blissito <blissitos@gmail.com>
2025-07-12 09:17:51 -0600

    test2

--- app/routes/home/Hero.tsx
+++ app/routes/home/Hero.tsx
@@ -12,7 +12,7 @@ export const Hero = () => {
       <div className="w-full md:w-[50%] ">
         <TextBlurEffect>
           <h1 className="text-4xl  md:text-5xl lg:text-6xl xl:text-[72px] leading-tight font-bold">
-            Saca los chispos digitales en línea{" "} 🫘
+            Saca los chispos digitales en línea{" "} 🫘 🧐
           </h1>
 
           <p className="text-iron text-xl lg:text-2xl xl:text-2xl font-extralight mb-6 md:mb-12 mt-2 md:mt-6">
