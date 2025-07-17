553a5cb097c2a0b47f8c8bcfdcc70ab6369e371a
oswaldo  martinez <oswalfut_96@hotmail.com>
2025-07-17 10:01:28 -0500

    upgrade nx project to taiwlind v4

--- vite.config.ts
+++ vite.config.ts
@@ -2,30 +2,17 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-// import { externalizeDeps } from "vite-plugin-externalize-deps";
 
 export default defineConfig({
   server: { port: 3000 },
 
-  plugins: [
-    reactRouter(),
-    tsconfigPaths(),
-    tailwindcss(),
-    //  externalizeDeps()
-  ],
+  plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
   build: {
-    // Habilitar compresión
     minify: "terser",
-    // Reducir el tamaño de los assets
     assetsInlineLimit: 4096,
-    // Habilitar source maps solo en desarrollo
     sourcemap: process.env.NODE_ENV === "development",
+    rollupOptions: {
+      external: ["react-hook-multipart"],
+    },
   },
-  // build: {
-  //   rollupOptions: {
-  //     // make sure to externalize deps that shouldn't be bundled
-  //     // into your library
-  //     external: ["react-hook-multipart"],
-  //   },
-  // },
 });
